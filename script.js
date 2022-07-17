598f1ec7be5201e9c5a923461595b6f6287eb6b2
Efe Dallı <104692278+efe3506@users.noreply.github.com>
2022-07-17 21:58:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,24 +7,63 @@ window.addEventListener("scroll", () => {
     header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+//smooth scroll JS
+const links = document.querySelectorAll('.nav-list a');
+
+function smoothScroll(e){
+    e.preventDefault();
+    const href = this.getAttribute('href');
+    document.querySelector(href).scrollIntoView({
+        behavior: "smooth",
+    });
+}
+
+for(let link of links){
+    link.addEventListener('click', smoothScroll);
+}
+
+
+
+
 //section-1 JS
 //icons
 
 //değişkenler
 const icons = document.querySelectorAll('.section-1 i');
-let i = 1;
+let iIcon = 1;
 
 //Tekrarlanan animasyon
 setInterval(() => {
-    i++;
+    iIcon++;
     const icon = document.querySelector('.section-1-icons .change');
     icon.classList.remove('change');
 
     //nextElementSibling son elemana geldikten sonra hata verir. Bunu engellemek için globalde bir i değişkeni tanımlayıp her tekrarda i'yi büyütüp koşul olarak icons.length'i geçmemesini sağladım
-    if(i > icons.length){
+    if(iIcon > icons.length){
         icons[0].classList.add('change');
-        i = 1;
+        iIcon = 1;
     } else{
         icon.nextElementSibling.classList.add('change');
     }
-}, 2500)
+}, 2500);
+
+
+//section-3 JS
+
+//set interval ile toggle cağır
+const arrows = document.querySelectorAll(".arrow-p");
+let iParagraph = 1;
+
+
+setInterval(() =>{
+    iParagraph++;
+    const arrow = document.querySelector(".arrow-container .change-arr")
+    arrow.classList.remove('change-arr');
+
+    if(iParagraph > arrows.length){
+        arrows[0].classList.add('change-arr');
+        iParagraph = 1;
+    } else{
+        arrow.nextElementSibling.classList.add('change-arr')
+    }
+}, 1000)
